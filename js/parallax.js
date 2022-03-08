6709f6f305b201abe523ddaeb61e5c8c4d779c42
Dissident <gonzalomedina23lj@gmail.com>
2022-03-08 18:17:50 -0300

    08/03/2022_1

---
+++ js/parallax.js
@@ -0,0 +1,22 @@
+let earth = document.getElementById('earth')
+let satellite = document.getElementById('satellite')
+let jupiter = document.getElementById('jupiter')
+let sun = document.getElementById('sun')
+let moon = document.getElementById('moon')
+let spaceship = document.getElementById('spaceship')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+
+    earth.style.top = value * 0.2 + 'px';
+    satellite.style.marginTop = value * -0.6 + 'px';
+    sun.style.marginRight = value * -0.3 + 'px'; 
+    sun.style.top = value * -0.2 + 'px';
+    moon.style.marginRight = value * -0.3 + 'px'; 
+    moon.style.marginTop = value * -0.4 + 'px';
+    moon.style.width = value * -1 + 'px';
+    jupiter.style.marginTop = value * -0.4 + 'px';
+    jupiter.style.marginLeft = value * -0.4 + 'px';
+    spaceship.style.marginLeft = value * 0.8 + 'px';
+    
+})
