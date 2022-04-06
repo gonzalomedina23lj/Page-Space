21a59ced2fd86c6f61cf7a83ef80c2d22ff92292
Dissident <gonzalomedina23lj@gmail.com>
2022-04-06 09:08:06 -0300

    06/04/2022_1

--- js/parallax.js
+++ js/parallax.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function(){
     sun.style.top = value * -0.2 + 'px';
     moon.style.marginRight = value * -0.3 + 'px'; 
     moon.style.marginTop = value * -0.4 + 'px';
-    moon.style.width = value * -1 + 'px';
+    /* moon.style.width = value * -10 + 'px'; */
     jupiter.style.marginTop = value * -0.4 + 'px';
     jupiter.style.marginLeft = value * -0.4 + 'px';
     spaceship.style.marginLeft = value * 0.8 + 'px';
